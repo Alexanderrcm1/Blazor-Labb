1dac383a9818020dd03236d7934bfbcb7e0cd046
Alexander Carlsson Musa <a.carlssons98@gmail.com>
2024-10-08 18:14:31 +0200

    Started on the new user page, made an EditForm and started with styling

--- Blazor-Labb/obj/Debug/net8.0/Blazor-Labb.AssemblyInfo.cs
+++ Blazor-Labb/obj/Debug/net8.0/Blazor-Labb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor-Labb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b5effcce9de02644c9173f8a0739f1081af298")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8c8fe0d78331ff26f0b21dcce29e744d39933c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor-Labb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor-Labb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
