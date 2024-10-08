bc0d54c537e4ce6a3f7b2a187ac8ff2a185cb7ac
Alexander Carlsson Musa <a.carlssons98@gmail.com>
2024-10-08 11:09:50 +0200

    Added Home, New Users and Users pages and linked them to the nav with navlink

--- Blazor-Labb/obj/Release/net8.0/Blazor-Labb.AssemblyInfo.cs
+++ Blazor-Labb/obj/Release/net8.0/Blazor-Labb.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor-Labb")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184862bbf54aea6a1ed1a7fba163cd96f0f2ee79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor-Labb")]
